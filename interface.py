6ee74096617dca71c384066a312e0a550615dd84
JustGinger888 <ethanmoffatt888@icloud.com>
2020-05-08 20:39:43 +0100

    comments

--- interface.py
+++ interface.py
@@ -8,22 +8,31 @@ cursor = db.cursor()
 
 
 # Functions
+
+## Initial Input Of Shopper ID To Get Corresponding DB Data
 def get_shopper_id():
+    # Initiate Loop
     shopper_id_found = True
     while shopper_id_found:
+        # Add List of valid Shopper IDs from DB
         shopperIDList = [row[0] for row in db.execute(idFindQuery)]
+        
+        #
         shopper_id = input('Enter Valid Shopper ID: ')
         if shopper_id in shopperIDList:
             print(get_menu_choice())
         else:
             print('Cannot find shopper ID, proceeding to exit...')
 
+## Gets, Displays and Compares the Menu Choices
 def get_menu_choice():
     loop = True
     while loop:
+        # Display Menu Options
         get_menu_items(menu_options, title)
         
-        choice = input("Enter your choice [1-5]: ")
+        # Check Menu Items
+        choice = input('Enter your choice [1-5]: ')
         if choice == 1:
             loop = True
         elif choice == 2:
@@ -31,13 +40,14 @@ def get_menu_choice():
         elif choice == 3:
             loop = True
         elif choice == 4:
-            loop = True
+            print('Not implemented')
         elif choice == 5:
             loop = False
         else:
             print 'Wrong menu selection, enter any key to try again...\n\n'
     return [choice]
 
+## Displays Title And Layout Of Choices
 def get_menu_items(menu_options, title):
         print(title)
         print(73 * "-")
