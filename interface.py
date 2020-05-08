513d2401cb33d7994d0c4596b63340e42d9f0f68
JustGinger888 <ethanmoffatt888@icloud.com>
2020-05-08 20:19:53 +0100

    intial complete

--- interface.py
+++ interface.py
@@ -1,57 +1,67 @@
-def get_menu_choice():
-    def print_menu(menu_options, title):       # Your menu design here
+import sqlite3
 
+
+# Database Declaration
+db = sqlite3.connect('C:/Users/ethan/Desktop/Databases/assessment.db')
+cursor = db.cursor()
+# Database Declaration
+
+
+# Functions
+def get_shopper_id():
+    shopper_id_found = True
+    while shopper_id_found:
+        shopperIDList = [row[0] for row in db.execute(idFindQuery)]
+        shopper_id = input('Enter Valid Shopper ID: ')
+        if shopper_id in shopperIDList:
+            print(get_menu_choice())
+        else:
+            print('Cannot find shopper ID, proceeding to exit...')
+
+def get_menu_choice():
+    loop = True
+    while loop:
+        get_menu_items(menu_options, title)
+        
+        choice = input("Enter your choice [1-5]: ")
+        if choice == 1:
+            loop = True
+        elif choice == 2:
+            loop = True
+        elif choice == 3:
+            loop = True
+        elif choice == 4:
+            loop = True
+        elif choice == 5:
+            loop = False
+        else:
+            print 'Wrong menu selection, enter any key to try again...\n\n'
+    return [choice]
+
+def get_menu_items(menu_options, title):
         print(title)
         print(73 * "-")
         for option in menu_options:
-            print option
+            print option[0]
+# Functions
 
-    loop = True
-    int_choice = -1
 
-    while loop:          # While loop which will keep going until loop = False
-        print_menu(menu_options, title)    # Displays menu
-        choice = input("Enter your choice [1-5]: ")
-
-        if choice == '1':
-            int_choice = 1
-            loop = False
-        elif choice == '2':
-            choice = ''
-            while len(choice) == 0:
-                choice = input("Enter custom folder name(s). It may be a list of folder's names (example: c:,d:\docs): ")
-            int_choice = 2
-            loop = False
-        elif choice == '3':
-            choice = ''
-            while len(choice) == 0:
-                choice = input("Enter a single filename of a file with custom folders list: ")
-            int_choice = 3
-            loop = False
-        elif choice == '4':
-            choice = ''
-            while len(choice) == 0:
-                choice = input("Enter a single filename of a conf file: ")
-            int_choice = 4
-            loop = False
-        elif choice == '5':
-            int_choice = -1
-            print("Exiting..")
-            loop = False  # This will make the while loop to end
-        else:
-            # Any inputs other than values 1-4 we print an error message
-            input("Wrong menu selection. Enter any key to try again..")
-    return [int_choice, choice]
-
+# Main Display Menu
 title = 'ORINOCO - SHOPPER MAIN MENU'
-
 menu_options = [
-    '1) Display your order history',
-    '2) Add an item to your basket',
-    '3) View your basket',
-    '4) Checkout',
-    '5) Exit'
-]
+    ['1) Display your order history','Hi'],
+    ['2) Add an item to your basket','Hi'],
+    ['3) View your basket','Hi'],
+    ['4) Checkout','Hi'],
+    ['5) Exit','Hi']]
+# Main Display Menu
 
 
-print(get_menu_choice())
+# Queries
+idFindQuery = 'SELECT shopper_id FROM shoppers'
+
+# Queries
+
+
+
+print(get_shopper_id())
